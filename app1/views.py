0a670bdfa30cf4c7f03ad6bdae39250b3f7b70ff
Jordanizquierdo <169169895+Jordanizquierdo@users.noreply.github.com>
2024-09-03 17:05:31 -0400

    part1
    incio app y proyecto

--- app1/views.py
+++ app1/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+
+def hola(request):
+    return HttpResponse("<h1>HOLA<h1>")
+
+def pag(request):
+    data = {"nombre":"Jordan"}
+    return render(request,'app1/index.html',data)
